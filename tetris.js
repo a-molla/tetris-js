f4a9454b971e23f1c1d21e94b3ec455aa49a1726
ahmad <ahmad.molla1711@gmail.com>
2019-09-11 14:50:52 -0400

    player rotation + remove continous lines

--- tetris.js
+++ tetris.js
@@ -6,10 +6,19 @@ context.scale(20, 20);
 context.fillStyle = "#000";
 context.fillRect(0, 0, 20, 20);
 
-// Represented as a 2D Matrix
-
-// T piece
-const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
+function arenaSweep() {
+  outer: for (let y = arena.length - 1; y > 0; y--) {
+    for (let x = 0; x < arena[y].length; x++) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+    // Remove the row from the arena, fill it with 0, add it to the top.
+    const row = arena.splice(y,1)[0].fill(0);
+    arena.unshift(row);
+    ++y;
+  }
+}
 
 function collide(arena, player) {
   const [matrix, position] = [player.matrix, player.pos];
@@ -34,6 +43,27 @@ function createMatrix(w, h) {
   return matrix;
 }
 
+function createPiece(type) {
+  switch (type) {
+    case "T":
+      return [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
+    case "O":
+      return [[2, 2], [2, 2]];
+    case "L":
+      return [[0, 3, 0], [0, 3, 0], [0, 3, 3]];
+    case "J":
+      return [[0, 4, 0], [0, 4, 0], [4, 4, 0]];
+    case "I":
+      return [[0, 5, 0, 0], [0, 5, 0, 0], [0, 5, 0, 0], [0, 5, 0, 0]];
+    case "S":
+      return [[0, 6, 6], [6, 6, 0], [0, 0, 0]];
+    case "Z":
+      return [[7, 7, 0], [0, 7, 7], [0, 0, 0]];
+    default:
+      return [];
+  }
+}
+
 function draw() {
   context.fillStyle = "#000";
   context.fillRect(0, 0, canvas.clientWidth, canvas.height);
@@ -45,7 +75,7 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
-        context.fillStyle = "red";
+        context.fillStyle = colors[value];
         context.fillRect(x + offset.x, y + offset.y, 1, 1);
       }
     });
@@ -54,9 +84,18 @@ function drawMatrix(matrix, offset) {
 
 const player = {
   pos: { x: 5, y: 5 },
-  matrix: matrix
+  matrix: createPiece("T")
 };
-
+const colors = [
+  null,
+  "red",
+  "blue",
+  "green",
+  "violet",
+  "purple",
+  "grey",
+  "yellow"
+];
 const arena = createMatrix(12, 20);
 
 function merge(arena, player) {
@@ -74,16 +113,60 @@ function playerDrop() {
   if (collide(arena, player)) {
     player.pos.y--;
     merge(arena, player);
-    player.pos.y = 0;
+    playerReset();
+    arenaSweep()
   }
   dropCounter = 0; // If a manual drop occurs, restart the drop delay.
 }
 
 function playerMove(direction) {
-    player.pos.x += direction;
-    if(collide(arena, player)){
-        player.pos.x -= direction;
+  player.pos.x += direction;
+  if (collide(arena, player)) {
+    player.pos.x -= direction;
+  }
+}
+
+function playerRotate(direction) {
+  const position = player.pos.x;
+  let offset = 1;
+
+  rotate(player.matrix, direction);
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > player.matrix[0].length) {
+      rotate(player.matrix, -direction);
+      player.pos.x = position;
+      return;
     }
+  }
+}
+
+function playerReset() {
+  const pieces = "ILJOTSZ";
+  player.matrix = createPiece(pieces[(pieces.length * Math.random()) | 0]);
+  player.pos.y = 0;
+  player.pos.x =
+    ((arena[0].length / 2) | 0) - ((player.matrix[0].length / 2) | 0);
+
+  // Clear arena if game over.
+  if (collide(arena, player)) {
+    arena.forEach(row => row.fill(0));
+  }
+}
+
+function rotate(matrix, dir) {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < y; x++) {
+      [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
+    }
+  }
+
+  if (dir > 0) {
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
 }
 
 let dropCounter = 0;
@@ -116,6 +199,14 @@ document.addEventListener("keydown", event => {
 
   if (keyCode === 40) {
     playerDrop();
+  }
+
+  if (keyCode === 81) {
+    playerRotate(-1);
+  }
+
+  if (keyCode === 87) {
+    playerRotate(1);
   }
 });
 
